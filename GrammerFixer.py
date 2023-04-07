554f633881c74faf4c005b2b66666100c6a5635a
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-08 02:45:10 +0530

    Created Grammar Correction script

---
+++ GrammerFixer.py
@@ -0,0 +1,9 @@
+from happytransformer import HappyTextToText as HappyTTT
+from happytransformer import TTSettings
+def Grammer_Fixer(Text):
+    Grammer = HappyTTT("T5","prithivida/grammar_error_correcter_v1")
+    config = TTSettings(do_sample=True, top_k=10, max_length=100)
+    corrected = Grammer.generate_text(Text, args=config)
+    print("Corrected Text: ", corrected.text)
+Text = "This is smple tet we how know this"
+Grammer_Fixer(Text)
